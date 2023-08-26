2534b3d756ecfb67eabf483dd8b5cd4970a851a2
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-08-26 21:18:40 +0530

    changes in data file

--- my-portfolio/src/Data/Projects.js
+++ my-portfolio/src/Data/Projects.js
@@ -3,10 +3,9 @@ var projects = {
     {
       image: "./images/projects/Modified-Tiffin-Service.png",
       project_name: "Modified Tiffin Service",
-      skills:
-        "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
+      skills: "HTML , CSS , JS , PHP , MySQL , Tailwind",
       content:
-        "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+        "This is our final DE project. In this project we came up with the unique idea to provide the tiffin service in unique way. This platform also provide the employeement to house women by that the housewomen can register there self on our platform and provide healthy and trasty food to customers.",
     },
     {
       image: "./images/projects/i-job.jpg",
@@ -19,34 +18,30 @@ var projects = {
     {
       image: "./images/projects/news-site.png",
       project_name: "News-site",
-      skills:
-        "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
-      content:
-        "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+      skills: "HTML , CSS , JS , PHP , MySQL",
+      content: "This is practise project by using the php",
     },
     {
       image: "./images/projects/mess-management.jpg",
       project_name: "Mess Managament",
       skills:
-        "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
+        "ReactJs , NodeJS , ExpressJS , MongoDB , Tailwind , Other ReactJS Framework",
       content:
-        "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+        "This project is for hackathon , which was held at VGEC college Ahmedabad. This project is regarding the mess management system of our college to manage the student's attendance , inventory etc. By this project oour team won the 3rd price in that hackathon.",
     },
     {
       image: "./images/projects/ipl-data-analysis.jpg",
       project_name: "Ipl data set Analysis",
-      skills:
-        "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
+      skills: "Python , Pandas , Numpy , Matplotlib, Seaborn",
       content:
-        "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+        "We make this research paper as a part of our 5th semester data mining submission. In which we perform certain preprocessing steps , data extraction on huge IPL data set and make research paper regarding the impact player in IPL.",
     },
     {
       image: "./images/projects/weather-project.png",
       project_name: "Weather Website",
-      skills:
-        "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
+      skills: "HTML , CSS , Tailwind , PHP , Weather-API",
       content:
-        "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+        "This Project is based on Weather api which tells weather forecast of 5 days , using weather api",
     },
   ],
 };
