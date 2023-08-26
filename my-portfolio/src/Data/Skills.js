c358b173737662d4f6dab3cd4a0b1fd0c8c35c0f
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-08-26 20:54:46 +0530

    skills data added

---
+++ my-portfolio/src/Data/Skills.js
@@ -0,0 +1,94 @@
+var skills = {
+  data: [
+    {
+      image: "./images/icons/html5.svg",
+      name: "HTML",
+    },
+    {
+      image: "./images/icons/css.svg",
+      name: "CSS",
+    },
+    {
+      image: "./images/icons/javascript.svg",
+      name: "JavaScript",
+    },
+    {
+      image: "./images/icons/java.svg",
+      name: "Java",
+    },
+    {
+      image: "./images/icons/php.svg",
+      name: "PHP",
+    },
+    {
+      image: "./images/icons/mysql-6.svg",
+      name: "MySQL",
+    },
+    {
+      image: "./images/icons/figma.svg",
+      name: "Figma",
+    },
+    {
+      image: "./images/icons/bootstrap.svg",
+      name: "Bootstrap",
+    },
+    {
+      image: "./images/icons/tailwind-css.svg",
+      name: "Tailwind",
+    },
+    {
+      image: "./images/icons/office.svg",
+      name: "Microsoft Office",
+    },
+    {
+      image: "./images/icons/git.svg",
+      name: "Git",
+    },
+    {
+      image: "./images/icons/html5.svg",
+      name: "HTML",
+    },
+    {
+      image: "./images/icons/css.svg",
+      name: "CSS",
+    },
+    {
+      image: "./images/icons/javascript.svg",
+      name: "JavaScript",
+    },
+    {
+      image: "./images/icons/java.svg",
+      name: "Java",
+    },
+    {
+      image: "./images/icons/php.svg",
+      name: "PHP",
+    },
+    {
+      image: "./images/icons/mysql-6.svg",
+      name: "MySQL",
+    },
+    {
+      image: "./images/icons/figma.svg",
+      name: "Figma",
+    },
+    {
+      image: "./images/icons/bootstrap.svg",
+      name: "Bootstrap",
+    },
+    {
+      image: "./images/icons/tailwind-css.svg",
+      name: "Tailwind",
+    },
+    {
+      image: "./images/icons/office.svg",
+      name: "Microsoft Office",
+    },
+    {
+      image: "./images/icons/git.svg",
+      name: "Git",
+    },
+  ],
+};
+
+export { skills };
