e0ec2c3d4e7896102f5aac32e6669242fb75ed37
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-08-26 20:54:16 +0530

    projects data added

---
+++ my-portfolio/src/Data/Projects.js
@@ -0,0 +1,54 @@
+var projects = {
+  data: [
+    {
+      image: "./images/projects/Modified-Tiffin-Service.png",
+      project_name: "Modified Tiffin Service",
+      skills:
+        "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
+      content:
+        "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+    },
+    {
+      image: "./images/projects/i-job.jpg",
+      project_name: "I - Job",
+      skills:
+        "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
+      content:
+        "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+    },
+    {
+      image: "./images/projects/news-site.png",
+      project_name: "News-site",
+      skills:
+        "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
+      content:
+        "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+    },
+    {
+      image: "./images/projects/mess-management.jpg",
+      project_name: "Mess Managament",
+      skills:
+        "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
+      content:
+        "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+    },
+    {
+      image: "./images/projects/ipl-data-analysis.jpg",
+      project_name: "Ipl data set Analysis",
+      skills:
+        "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
+      content:
+        "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+    },
+    {
+      image: "./images/projects/weather-project.png",
+      project_name: "Weather Website",
+      skills:
+        "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
+      content:
+        "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+    },
+  ],
+};
+
+export { projects };
