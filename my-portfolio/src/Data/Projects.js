d761500028c7b938b175c2e154549f58cbafa2c3
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-08-27 19:41:08 +0530

    project details added

--- my-portfolio/src/Data/Projects.js
+++ my-portfolio/src/Data/Projects.js
@@ -6,6 +6,7 @@ var projects = {
       skills: "HTML , CSS , JS , PHP , MySQL , Tailwind",
       content:
         "This is our final DE project. In this project we came up with the unique idea to provide the tiffin service in unique way. This platform also provide the employeement to house women by that the housewomen can register there self on our platform and provide healthy and trasty food to customers.",
+      projectLink: "https://github.com/ANIKET1074/ADA-Project",
     },
     {
       image: "./images/projects/i-job.jpg",
@@ -14,12 +15,14 @@ var projects = {
         "HTML , CSS , JS , NodeJS , ExpressJS , Pug , MongoDB , Bootstrap",
       content:
         "This is our college project for ADA subject.This project is done in November of 2022. This project is to connect companies and employee on one platform and gave the best ppossible opportunities to employee for their placements.",
+      projectLink: "https://github.com/ANIKET1074/ADA-Project",
     },
     {
       image: "./images/projects/news-site.png",
       project_name: "News-site",
       skills: "HTML , CSS , JS , PHP , MySQL",
       content: "This is practise project by using the php",
+      projectLink: "https://github.com/ANIKET1074/News-site",
     },
     {
       image: "./images/projects/mess-management.jpg",
@@ -28,6 +31,7 @@ var projects = {
         "ReactJs , NodeJS , ExpressJS , MongoDB , Tailwind , Other ReactJS Framework",
       content:
         "This project is for hackathon , which was held at VGEC college Ahmedabad. This project is regarding the mess management system of our college to manage the student's attendance , inventory etc. By this project oour team won the 3rd price in that hackathon.",
+      projectLink: "https://github.com/ANIKET1074/Mess_Management",
     },
     {
       image: "./images/projects/ipl-data-analysis.jpg",
@@ -35,6 +39,7 @@ var projects = {
       skills: "Python , Pandas , Numpy , Matplotlib, Seaborn",
       content:
         "We make this research paper as a part of our 5th semester data mining submission. In which we perform certain preprocessing steps , data extraction on huge IPL data set and make research paper regarding the impact player in IPL.",
+      projectLink: "https://github.com/ANIKET1074/ADA-Project",
     },
     {
       image: "./images/projects/weather-project.png",
@@ -42,6 +47,7 @@ var projects = {
       skills: "HTML , CSS , Tailwind , PHP , Weather-API",
       content:
         "This Project is based on Weather api which tells weather forecast of 5 days , using weather api",
+      projectLink: "https://github.com/ANIKET1074/Weather-Website",
     },
   ],
 };
