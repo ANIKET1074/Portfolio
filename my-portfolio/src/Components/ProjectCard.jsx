f7d914619743de6b827acbbfb073d8179c8b7fcf
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-08-27 19:43:15 +0530

    Project card added

--- my-portfolio/src/Components/ProjectCard.jsx
+++ my-portfolio/src/Components/ProjectCard.jsx
@@ -1,21 +1,30 @@
 import React from "react";
+import "../Pages/Project/Project.css";
 
-const ProjectCard = (props) => {
+const ProjectCard = ({ image, pname, skills, content, projectlink }) => {
   return (
     <>
-      <div className="flex flex-col border shadow-2xl rounded-xl bg-[white] w-[26rem] h-[20rem] items-center">
+      <div className="flex flex-col border shadow-2xl rounded-xl bg-[white]  items-center projectCard">
         <img
-          src={props.image}
+          src={image}
           alt=""
           className="w-[100%] flex h-[70%] rounded-xl p-4 bg-[white]"
         />
         <div className="content flex-col gap-2">
           <h2 className="text-center bg-[white] flex-col font-semibold items-center text-[black]">
-            {props.pname}
+            {pname}
           </h2>
           <span className="text-center flex text-sm px-3 bg-[white] text-[black] mt-2">
-            {props.skills}
+            {skills}
           </span>
+        </div>
+
+        <div className="projectDesc">
+          <h2>{pname}</h2>
+          <p>{content}</p>
+          <a href={projectlink} rel="noreferrer" target="_blank">
+            Code &lt;/&gt;
+          </a>
         </div>
       </div>
     </>
