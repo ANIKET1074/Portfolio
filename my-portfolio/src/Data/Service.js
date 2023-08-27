ca27a354e0e750b3b13c612e2d01280880818d7b
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-08-27 19:41:40 +0530

    service file added

---
+++ my-portfolio/src/Data/Service.js
@@ -0,0 +1,30 @@
+var services = {
+  data: [
+    {
+      image: "./images/svg-icons/code.svg",
+      name: "Frontend Developer",
+    },
+    {
+      image: "./images/svg-icons/database.svg",
+      name: "Backend Developer",
+    },
+    {
+      image: "./images/svg-icons/web-development.svg",
+      name: "Web Design",
+    },
+    {
+      image: "./images/svg-icons/Database-management.svg",
+      name: "Database Management",
+    },
+    {
+      image: "./images/svg-icons/analytics.svg",
+      name: "Data Analytics",
+    },
+    {
+      image: "./images/svg-icons/writing.svg",
+      name: "Content Writing",
+    },
+  ],
+};
+
+export { services };
