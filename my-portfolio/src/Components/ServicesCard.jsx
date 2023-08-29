15ced52920a9025dfb807f4688dd8754e27322f8
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-08-29 11:15:14 +0530

    Services Card

--- my-portfolio/src/Components/ServicesCard.jsx
+++ my-portfolio/src/Components/ServicesCard.jsx
@@ -4,9 +4,9 @@ const ServicesCard = ({ image, name }) => {
   return (
     <>
       <div className="service w-[10rem] h-[10rem] border border-[#5219d1] rounded-[0.5rem] justify-center flex flex-col gap-3 items-center">
-        <img src={image} alt="" className="w-[2.5rem]" />
+        <img src={image} alt="" className="w-[2.65rem]" />
 
-        <h2 className="text-[0.85rem] text-center">{name}</h2>
+        <h2 className="text-[0.85rem] font-semibold text-center">{name}</h2>
       </div>
     </>
   );
